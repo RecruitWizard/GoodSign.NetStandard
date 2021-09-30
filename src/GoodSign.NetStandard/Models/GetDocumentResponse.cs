db2af6799230873a780c6d5d588142fa1bf48712
Ben Macpherson <ben@uniquecomputing.com.au>
2021-09-30 22:58:22 +1000

    Add Signers to Get Document Request

--- src/GoodSign.NetStandard/Models/GetDocumentResponse.cs
+++ src/GoodSign.NetStandard/Models/GetDocumentResponse.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace GoodSign.NetStandard.Models
 {
@@ -14,11 +15,36 @@ namespace GoodSign.NetStandard.Models
         [JsonProperty("Webhook")]
         public string Webhook { get; set; }
 
+        [JsonProperty("signers")]
+        public List<GetSigner> Signers { get; set; }
+
         [JsonProperty("download_pdf")]
         public string MainDocumentDownloadUrl { get; set; }
 
         [JsonProperty("download_zip")]
         public string OtherDocumentsDownloadUrl { get; set; }
+    }
+
+    public class GetSigner
+    {
+        [JsonProperty("contact")]
+        public Contact Contact { get; set; }
+        [JsonProperty("complete")]
+        public bool Complete { get; set; }
+        [JsonProperty("reminderdays")]
+        public int ReminderDays { get; set; }
+        [JsonProperty("key")]
+        public string Key { get; set; }
+        [JsonProperty("emailStatus")]
+        public string EmailStatus { get; set; }
+    }
+
+    public class Contact
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("email")]
+        public string Email { get; set; }
     }
 
     public class GetDocumentResponse
